2be3a9e44a3541dedccd1ff19a5e4e73a7268c4e
Ilya Sarbey <36928846+IlyaBOT@users.noreply.github.com>
2026-09-10 15:11:20 +0300

    Mark macOS SwiftUI interface as Monterey or newer

--- BooruBar/ContentView.swift
+++ BooruBar/ContentView.swift
@@ -8,6 +8,7 @@
 import AppKit
 import SwiftUI
 
+@available(macOS 12.0, *)
 struct ContentView: View {
     @ObservedObject private var settingsStore: SettingsStore
     @StateObject private var viewModel: GalleryViewModel
@@ -288,6 +289,7 @@ struct ContentView: View {
     }
 }
 
+@available(macOS 12.0, *)
 #Preview {
     ContentView(settingsStore: SettingsStore())
 }
